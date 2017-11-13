381f19223d854f13a31a7a16e5b258f028d073a2
Rachel Pearse <rpsongs33@gmail.com>
2017-11-13 06:31:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file
+
+
+//var newArray = arr.filter(callback[, thisArg])
+
+const drivers = [
+  {
+    name: 'Bobby',
+    hometown: 'Pittsburgh' },
+  {
+    name: 'Sammy',
+    hometown: 'New York' } ,
+  {
+    name: 'Sally',
+    hometown: 'Cleveland' },
+  {
+    name: 'Annette',
+    hometown: 'Los Angeles' },
+  {
+    name: 'Bobby',
+    hometown: 'Tampa Bay' }
+];
+findMatching() {}
